68357d89a672d475aee9f43d7a5f2d0d6b1609df
mapaloCat <mapalo2008@gmail.com>
2019-12-07 14:02:06 +0100

    mouse brain data

--- GeneVisualization/implementation.py
+++ GeneVisualization/implementation.py
@@ -268,9 +268,67 @@ class RaycastRendererImplementation(RaycastRenderer):
 
     # TODO: Implement function to render multiple energy volumes and annotation volume as a silhouette.
     def render_mouse_brain(self, view_matrix: np.ndarray, annotation_volume: Volume, energy_volumes: dict,
-                           image_size: int, image: np.ndarray):
+                           image_size: int, image: np.ndarray, trilinear: bool=False):
         # TODO: Implement your code considering these volumes (annotation_volume, and energy_volumes)
-        pass
+        # Clear the image
+        self.clear_image()
+
+        # U vector. See documentation in parent's class
+        u_vector = view_matrix[0:3]
+
+        # V vector. See documentation in parent's class
+        v_vector = view_matrix[4:7]
+
+        # View vector. See documentation in parent's class
+        view_vector = view_matrix[8:11]
+
+        # Center of the image. Image is squared
+        image_center = image_size / 2
+
+        # Center of the volume (3-dimensional)
+        volume_center = [annotation_volume.dim_x / 2, annotation_volume.dim_y / 2, annotation_volume.dim_z / 2]
+        volume_maximum = annotation_volume.get_maximum()
+
+        # Define a step size to make the loop faster
+        step = 2 if self.interactive_mode else 1
+
+        for i in range(0, image_size, step):
+            for j in range(0, image_size, step):
+                # Get the voxel coordinate X
+                voxel_coordinate_x = u_vector[0] * (i - image_center) + v_vector[0] * (j - image_center) + \
+                                     volume_center[0]
+
+                # Get the voxel coordinate Y
+                voxel_coordinate_y = u_vector[1] * (i - image_center) + v_vector[1] * (j - image_center) + \
+                                     volume_center[1]
+
+                # Get the voxel coordinate Z
+                voxel_coordinate_z = u_vector[2] * (i - image_center) + v_vector[2] * (j - image_center) + \
+                                     volume_center[2]
+                # Get voxel value
+                if trilinear:
+                    value = trilinear_interpolation(annotation_volume, voxel_coordinate_x, voxel_coordinate_y,
+                                                    voxel_coordinate_z)
+                else:
+                    value = get_voxel(annotation_volume, voxel_coordinate_x, voxel_coordinate_y, voxel_coordinate_z)
+
+                # Normalize value to be between 0 and 1
+                red = value / volume_maximum
+                green = red
+                blue = red
+                alpha = 1.0 if red > 0 else 0.0
+
+                # Compute the color value (0...255)
+                red = math.floor(red * 255) if red < 255 else 255
+                green = math.floor(green * 255) if green < 255 else 255
+                blue = math.floor(blue * 255) if blue < 255 else 255
+                alpha = math.floor(alpha * 255) if alpha < 255 else 255
+
+                # Assign color to the pixel i, j
+                image[(j * image_size + i) * 4] = red
+                image[(j * image_size + i) * 4 + 1] = green
+                image[(j * image_size + i) * 4 + 2] = blue
+                image[(j * image_size + i) * 4 + 3] = alpha
 
 
 class GradientVolumeImpl(GradientVolume):
